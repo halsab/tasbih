03354eeb1cc5a8ae1fdaebb42ecea8d8d4d0289c
khalil_sabirov <SabirovKhA@mosreg.ru>
2022-12-19 11:51:47 +0300

    Created project

---
+++ tasbih/tasbihApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasbihApp.swift
+//  tasbih
+//
+//  Created by Khalil Sabirov on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct tasbihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
