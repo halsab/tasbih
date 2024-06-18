1428d32747eba96410d779e32c6caaaff8fadaae
khalil_sabirov <SabirovKhA@mosreg.ru>
2024-06-18 11:53:32 +0300

    Added progress view

--- tasbih/View/CountScreen/View/HeaderView.swift
+++ tasbih/View/CountScreen/View/HeaderView.swift
@@ -12,32 +12,38 @@ struct HeaderView: View {
     @EnvironmentObject private var cm: CountManager
 
     var body: some View {
-        HStack {
-            Menu {
-                ForEach(LoopSize.allCases, id: \.self) { loopSize in
-                    Button {
-                        cm.loopSize = loopSize.rawValue
-                    } label: {
-                        Label(loopSize.title,
-                              systemImage: cm.loopSize == loopSize.rawValue
-                              ? loopSize.selectedIconName : "")
+        VStack(spacing: 6) {
+            HStack {
+                RollingText(value: $cm.totalCounts,
+                            font: .app.lTitle,
+                            foregroundColor: Color.app.tint)
+
+                Spacer()
+
+                Menu {
+                    ForEach(LoopSize.allCases, id: \.self) { loopSize in
+                        Button {
+                            cm.loopSize = loopSize.rawValue
+                        } label: {
+                            Label(loopSize.title,
+                                  systemImage: cm.loopSize == loopSize.rawValue
+                                  ? loopSize.selectedIconName : "")
+                        }
                     }
+                } label: {
+                    Text("\(cm.loopSize)")
+                        .foregroundStyle(Color.app.tint)
+                        .font(.app.mTitle)
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 12)
+                        .background(.ultraThinMaterial)
+                        .clipShape(.capsule)
                 }
-            } label: {
-                Text("\(cm.loopSize)")
-                    .foregroundStyle(Color.app.tint)
-                    .font(.app.mTitle)
-                    .padding(.vertical, 6)
-                    .padding(.horizontal, 12)
-                    .background(.ultraThinMaterial)
-                    .clipShape(.capsule)
             }
 
-            Spacer()
-
-            RollingText(value: $cm.totalCounts,
-                        font: .app.lTitle,
-                        foregroundColor: Color.app.tint)
+            ProgressView(value: Double(cm.currentLoopCount), total: Double(cm.loopSize))
+                .tint(.app.tint)
+                .animation(.easeInOut, value: cm.currentLoopCount)
         }
     }
 }
