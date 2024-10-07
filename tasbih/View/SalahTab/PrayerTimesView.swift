50a36daba989ce8090d1c2d01f18984618314cfe
khalil_sabirov <SabirovKhA@mosreg.ru>
2024-10-07 16:00:36 +0300

    Added remaining time calculation

--- tasbih/View/SalahTab/PrayerTimesView.swift
+++ tasbih/View/SalahTab/PrayerTimesView.swift
@@ -11,13 +11,23 @@ struct PrayerTimesView: View {
     
     @State private var times: [PrayerTime] = []
     @State private var currentTime: PrayerTimeType = .fajr
+    @State private var nextTime: PrayerTime = .init(type: .fajr)
+    @State private var interval: Double = 0
+    @State private var remainingTime = "00:00:00"
+    @State private var timer: Timer?
     
     private let calculator = PrayerTimesCalculator(coordinate: .init(
         latitude: 55.7887, longitude: 49.1221
     ))
     
     var body: some View {
-        timesView(times: times)
+        VStack(alignment: .trailing) {
+            timesView(times: times)
+            
+            Text(remainingTime)
+                .monospaced()
+                .padding(.horizontal)
+        }
         .padding(.horizontal, 72)
         .onAppear {
             times = calculator.prayerTimes()
@@ -26,9 +36,29 @@ struct PrayerTimesView: View {
                 if time.date <= .now {
                     currentTime = time.type
                 } else {
+                    nextTime = time
                     break
                 }
             }
+            
+            interval = Double(nextTime.date.timeIntervalSince(.now))
+            
+            timer?.invalidate()
+            timer = nil
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                if interval > 0 {
+                    interval -= 1
+                    
+                    let rawMinutes = (interval / 60).rounded(.down)
+                    let hours = (rawMinutes / 60).rounded(.down)
+                    let minutes = rawMinutes.truncatingRemainder(dividingBy: 60).rounded(.down)
+                    let seconds = interval.truncatingRemainder(dividingBy: 60).rounded(.down)
+                    remainingTime = String(format: "%02.0f:%02.0f:%02.0f", hours, minutes, seconds)
+                } else {
+                    timer?.invalidate()
+                    timer = nil
+                }
+            })
         }
     }
     
@@ -63,4 +93,46 @@ struct PrayerTimesView: View {
 
 #Preview {
     PrayerTimesView()
+}
+
+class TimerModel: ObservableObject {
+    @Published var timerString = "00:00"
+    
+    private var countdownTime = 60 // seconds
+    private var timer: Timer?
+    private var totalTime = 60
+    
+    func startTimer() {
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                self.updateTimer()
+            }
+        }
+    }
+    
+    func pauseTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func resetTimer() {
+        pauseTimer()
+        countdownTime = totalTime
+        updateTimerString()
+    }
+    
+    private func updateTimer() {
+        if countdownTime > 0 {
+            countdownTime -= 1
+            updateTimerString()
+        } else {
+            pauseTimer()
+        }
+    }
+    
+    private func updateTimerString() {
+        let minutes = countdownTime / 60
+        let seconds = countdownTime % 60
+        timerString = String(format: "%02d:%02d", minutes, seconds)
+    }
 }
