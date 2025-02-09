7245a6b4d167b65c53ef560948f66eea05c12f89
khalil_sabirov <SabirovKhA@mosreg.ru>
2025-02-09 21:00:47 +0300

    feat: added central tap view

--- Module/Sources/Module/Simplified/Screen/CountScreen.swift
+++ Module/Sources/Module/Simplified/Screen/CountScreen.swift
@@ -14,6 +14,7 @@ public struct CountScreen: View {
     
     public var body: some View {
         ContentView(viewModel: viewModel)
+            .safeAreaPadding()
     }
     
     public init(modelContext: ModelContext) {
@@ -67,7 +68,7 @@ private extension CountScreen {
         func MainState() -> some View {
             VStack {
                 MainStateHeaderView(viewModel: viewModel)
-                //                    MainStateCentralView()
+                MainStateCentralView(viewModel: viewModel)
                 //                    MainStateFooterView()
             }
         }
@@ -85,6 +86,8 @@ private extension CountScreen {
         }
     }
 }
+
+// MARK: - MainStateHeaderView
 
 private extension CountScreen {
     struct MainStateHeaderView: View {
@@ -230,6 +233,76 @@ private extension CountScreen {
                 .contentTransition(.numericText())
                 .font(.app.font(.xxxl, .bold))
                 .foregroundStyle(Color.app.tint)
+        }
+    }
+}
+
+// MARK: - MainStateCentralView
+
+private extension CountScreen {
+    private struct MainStateCentralView: View {
+        @Bindable var viewModel: ViewModel
+        
+        var body: some View {
+            if let zikr = viewModel.selectedZikr {
+                ZStack {
+                    AnimationView(zikr: zikr)
+                    
+                    Image.app.button.count
+                        .font(.system(size: 100))
+                        .foregroundStyle(Color.shape(.app.tint))
+                        .symbolEffect(.bounce, options: .nonRepeating.speed(2), value: zikr.count)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onTapGesture {
+                    zikr.count += 1
+                }
+            } else {
+                EmptyView()
+            }
+        }
+        
+        struct AnimationView: View {
+            @Bindable var zikr: ZikrModel
+            
+            @State private var pulsedHearts: [HeartParticleModel] = []
+            
+            var body: some View {
+                TimelineView(.animation(minimumInterval: 0.5, paused: false)) { _ in
+                    Canvas { context, size in
+                        handleCanvas(context: context, size: size)
+                    } symbols: {
+                        CanvasSymbols()
+                    }
+                }
+                .blur(radius: 15)
+                .onChange(of: zikr.count) {
+                    addPulsedHeart()
+                }
+            }
+            
+            @ViewBuilder
+            private func CanvasSymbols() -> some View {
+                ForEach(pulsedHearts) {
+                    PulsedHeartView()
+                        .id($0.id)
+                }
+            }
+            
+            private func handleCanvas(context: GraphicsContext, size: CGSize) {
+                pulsedHearts
+                    .compactMap { context.resolveSymbol(id: $0.id) }
+                    .forEach { context.draw($0, at: .init(x: size.width / 2, y: size.height / 2)) }
+            }
+            
+            private func addPulsedHeart() {
+                let pulsedHeart = HeartParticleModel()
+                pulsedHearts.append(pulsedHeart)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    pulsedHearts.removeAll(where: { $0.id == pulsedHeart.id })
+                }
+            }
         }
     }
 }
