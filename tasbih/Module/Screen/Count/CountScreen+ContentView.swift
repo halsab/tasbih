1b739d7fc0480f07eb7424363f8c55afa3ee8924
khalil_sabirov <SabirovKhA@mosreg.ru>
2025-02-19 22:37:58 +0300

    ref: deleted old code

--- tasbih/Module/Screen/Count/CountScreen+ContentView.swift
+++ tasbih/Module/Screen/Count/CountScreen+ContentView.swift
@@ -11,43 +11,7 @@ extension CountScreen {
     struct ContentView: View {
         @Bindable var countService: CountService
         
-        @State private var showFirstZikrCreationAlert = false
-        @State private var firstZikrName = ""
-        
         var body: some View {
-            switch countService.contentState {
-            case .empty: EmptyState()
-            case .main: MainState()
-            }
-        }
-        
-        @ViewBuilder
-        func EmptyState() -> some View {
-            Button {
-                showFirstZikrCreationAlert.toggle()
-            } label: {
-                Label {
-                    Text(String.text.button.createFirstZikr)
-                } icon: {
-                    Image.app.icon.plus
-                }
-                .font(.app.font(.m, weight: .semibold))
-            }
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.extraLarge)
-            .tint(.app.tint.primary)
-            .alert(String.text.alert.createFirstZikr, isPresented: $showFirstZikrCreationAlert) {
-                ZikrCreationAlertView(name: $firstZikrName, isValid: {
-                    countService.isNewZikrNameValid(firstZikrName)
-                }, action: {
-                    countService.createZikr(name: firstZikrName)
-                })
-            }
-        }
-        
-        @ViewBuilder
-        func MainState() -> some View {
             VStack {
                 HeaderView(countService: countService)
                 CentralView(countService: countService)
